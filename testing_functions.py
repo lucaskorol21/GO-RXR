b0c0e3fdaac8a9e4807710f48aec19b9bc7113ee
lucaskorol21 <74730122+lucaskorol21@users.noreply.github.com>
2022-05-12 15:33:32 -0600

    Optical profile test

--- testing_functions.py
+++ testing_functions.py
@@ -2,7 +2,7 @@ from math import sin
 import numpy as np
 from scipy import interpolate
 from scipy import optimize
-import import Pythonreflectivity as pr
+import Pythonreflectivity as pr
 from material_model import *
 
 
@@ -12,4 +12,14 @@ from material_model import *
 
 if __name__ == "__main__":
 
-    print('hello')
+    mag_optical_profile = np.loadtxt('mag_optical_profile')
+    optical_profile = np.loadtxt('optical_constant')
+
+    t1 = optical_profile[:,0]
+    d1 = optical_profile[:, 1]
+    b1 = optical_profile[:, 3]
+    print(d1)
+    plt.figure()
+    plt.plot(t1,b1,'.')
+    plt.plot(t1,d1,'.')
+    plt.show()
