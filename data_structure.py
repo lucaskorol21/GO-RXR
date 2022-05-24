3191ad31f57288e033e8e14e57dfb8379167bcc3
lucaskorol21 <74730122+lucaskorol21@users.noreply.github.com>
2022-05-24 15:58:54 -0600

    Implementation of data structure

---
+++ data_structure.py
@@ -0,0 +1,8 @@
+# The purpose of this set of python functions is to create the data structure that will be in my python
+# reflectivity software.
+
+# Initial conversations revealed that HDF5 or ASCII are good options
+# Personally, I think that HDF5 is the best option
+
+if __name__ == "__main__":
+    print('hello')
