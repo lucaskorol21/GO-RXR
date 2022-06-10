856ca163d1342546915a1b4c50a38fc5f6618906
lucaskorol21 <74730122+lucaskorol21@users.noreply.github.com>
2022-06-10 15:08:05 -0600

    Working Progress of ASCII to HDF5

--- data_structure.py
+++ data_structure.py
@@ -1010,7 +1010,7 @@ def ConvertASCIItoHDF5(fname):
 
 
 def ReadDataASCII(fname):
-    print()
+    print('bye')
     """
     Purpose: Read .all file that contains sample and experimental information
     :param fname: Name of file contatining related information
