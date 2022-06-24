7d193a05af2ae126aeb7c91d5ea89b8fb02746ce
lucaskorol21 <74730122+lucaskorol21@users.noreply.github.com>
2022-06-24 11:48:55 -0600

    Working version for all variable types

--- GlobalOptimization.py
+++ GlobalOptimization.py
@@ -59,7 +59,7 @@ def selectOptimize(fname):
                         quit()
                 scans.append(scan)
     scans = [int(scan) for scan in scans]
-    print(scans)
+
 
 
 
@@ -70,7 +70,7 @@ def selectOptimize(fname):
 
     number_layers = len(sample.structure)
 
-    cont = 'y'
+    cont = 'Y'
     while cont.upper() == 'Y':
         layer = input('Select layer you would like to optimize (0-'+str(number_layers-1)+"): ")
         if layer.upper() == 'EXIT':
@@ -263,15 +263,149 @@ def selectOptimize(fname):
                         else:
                             again = 'N'
                     val = input('Would you like to select another element (y/n)?')
+        elif prop.upper() == "POLYMORPHOUS":
+            b= []
+            for ele in elements:
+                if len(sample.structure[layer][ele].polymorph) > 0:
+                    b.append(ele)
+            poly_cont = 'Y'
+            while poly_cont.upper() == 'Y' and len(b) > 0:
+                poly_ele = input('Select the polymorph '+ str(b) +' you would liked to vary: ')
+                if poly_ele.upper() == 'EXIT':
+                    quit()
+
+                num_poly = len(b)
+
+                while poly_ele not in b:
+                    poly_ele = input('Please select a polymorph ' + str(b) + ' : ')
+                    if poly_ele.upper() == 'EXIT':
+                        quit()
+
+
+                polymorph = sample.structure[layer][poly_ele].polymorph
+                whichPoly = input('Select polymorph (' + str(polymorph) + ') for which density you would like to vary?')
+                if whichPoly.upper() == 'EXIT':
+                    quit()
+                while whichPoly not in polymorph:
+                    whichPoly = input('Select polymorph (' + str(polymorph) + ') for which density you would like to vary?')
+                    if whichPoly.upper() == 'EXIT':
+                        quit()
+
+                lw = input("Select the lower limit of the polymorph ratio (0-1): ")
+                if lw.upper() == 'EXIT':
+                    quit()
+                up = input("Select the upper limit of the polymorph ratio (0-1): ")
+                if up.upper() == 'EXIT':
+                    quit()
+
+                while(float(lw)>float(up) or float(lw)<0 or float(up) > 1):
+                    if float(lw)>float(up):
+                        lw = input("Make sure that your lower bound is smaller than the upper bound. Please select a new lower limit: ")
+                        if lw.upper() == 'EXIT':
+                            quit()
+                        up = input("Please select a new upper bound: ")
+                        if up.upper() == 'EXIT':
+                            quit()
+                    if float(lw) < 0:
+                        lw = input("Please select a lower limit between 0 and 1: ")
+                        if lw.upper() == 'EXIT':
+                            quit()
+                    if float(up) > 1:
+                        up = input("Please select an upper limit between 0 and 1: ")
+                        if up.upper() == 'EXIT':
+                            quit()
+
+                lowerbound.append(lw)
+                upperbound.append(up)
+
+                sample_params.append([layer, prop.upper(), poly_ele, whichPoly])
+                # As of right now we will assume that we can have a maximum of 2 polymorphs
+
+                if len(b) != 0:
+                    b.remove(poly_ele)
+                    poly_cont = input('Would you like to vary another polymorph in the same layer (y/n): ')
+                    if poly_cont.upper() == 'EXIT':
+                        quit()
+
+        elif prop.upper() == 'MAGNETIC':
+            my_mag = list()
+            for ele in elements:
+                if len(sample.structure[layer][ele].mag_density) > 0:
+                    my_mag.append(ele)
+
+            mag_ele = input('Select magnetic element you would like to vary (' + str(my_mag) + ': ')
+            if mag_ele.upper() == 'EXIT':
+                quit()
+            while mag_ele not in my_mag:
+                mag_ele = input('Please select an element element in (' + str(my_mag) + ': ')
+                if mag_ele.upper() == 'EXIT':
+                    quit()
+
+            mag_poly = list(sample.structure[layer][mag_ele].polymorph)
+            mag_cont = 'Y'
+            while(mag_cont.upper() == 'Y' and len(my_mag)>0):
+                if len(mag_poly) > 0:
+                    mag_poly_cont = 'Y'
+                    while(mag_poly_cont.upper() == 'Y' and len(mag_poly)>0):
+                        whichMag = input('Select which polymorph ('+str(mag_poly)+') for which you would like to vary the magnetic density: ')
+                        if whichMag.upper() == 'EXIT':
+                            quit()
+                        while whichMag not in mag_poly:
+                            whichMag = input('Select which polymorph ('+str(mag_poly)+') for which you would like to vary the magnetic density: ')
+                            if whichMag.upper() == 'EXIT':
+                                quit()
+
+                        lw = input('Enter the lower bound of the magnetic density of order mol/cm^3: ')
+                        if lw.upper() == 'EXIT':
+                            quit()
+                        up = input('Enter the lower bound of the magnetic density of order mol.cm^3: ')
+                        if up.upper() == 'EXIT':
+                            quit()
+                        while float(lw) > float(up):
+                            lw = input('Make sure lower bound is smaller than upper bound. Enter lower bound again: ')
+                            if lw.upper() == 'EXIT':
+                                quit()
+                            up = input('Enter upper bound: ')
+                            if up.upper() == 'EXIT':
+                                quit()
+                        lowerbound.append(lw)
+                        upperbound.append(up)
+
+                        sample_params.append([layer, prop.upper(), mag_ele, whichMag])
+                        if len(mag_poly) != 0:
+                            mag_poly_cont = input('Would you like to vary another polymorph magnetic density (y/n)?')
+                            if mag_poly_cont.upper() == 'EXIT':
+                                quit()
+                            mag_poly.remove(whichMag)
+
+                else:
+                    lw = input('Enter the lower bound of the magnetic density of order mol/cm^3: ')
+                    if lw.upper() == 'EXIT':
+                        quit()
+                    up = input('Enter the lower bound of the magnetic density of order mol.cm^3: ')
+                    if up.upper() == 'EXIT':
+                        quit()
+                    while float(lw) > float(up):
+                        lw = input('Make sure lower bound is smaller than upper bound. Enter lower bound again: ')
+                        if lw.upper() == 'EXIT':
+                            quit()
+                        up = input('Enter upper bound: ')
+                        if up.upper() == 'EXIT':
+                            quit()
+                    lowerbound.append(lw)
+                    upperbound.append(up)
+                    sample_params.append([layer, prop.upper(), mag_ele])
+                if len(my_mag) != 0:
+                    my_mag.remove(mag_ele)
+                    mag_cont = input('Would you like to select another magnetic element to vary (y/n)?')
+                    if mag_cont.upper() == 'EXIT':
+                        quit()
 
         cont = input('Would you liked to select another layer (y/n): ')
 
 
 
-
-
-
-    return
+    return data_info[scans], data, sims, sample_params, lowerbound, upperbound
 
 
 if __name__ == "__main__":
