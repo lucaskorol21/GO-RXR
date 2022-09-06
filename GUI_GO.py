d2438f096a2ab46a2750710eab50e5b1598401d2
lucaskorol21 <74730122+lucaskorol21@users.noreply.github.com>
2022-09-06 16:42:39 -0600

    Starting work on GO GUI

--- GUI_GO.py
+++ GUI_GO.py
@@ -1,58 +1,108 @@
 from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import Qt
 import sys
 
-class TableWidget(QTableWidget):
+# Element table widget
+class elementTable(QTableWidget):
+    def __init__(self):
+        super().__init__(3,5)
+
+# Compound table widget
+class compoundTable(QTableWidget):
     def __init__(self):
         super().__init__(1, 5)
-        self.setHorizontalHeaderLabels(['Formula','Thickness (A)','Density (g/cm^3)','Roughness (A)', 'Linked Roughness'])
-        self.verticalHeader().setDefaultSectionSize(25)
-        self.horizontalHeader().setDefaultSectionSize(150)
-    def _addRow(self):
-        rowCount = self.rowCount()
-        self.insertRow(rowCount)
-    def _removeRow(self):
-        if self.rowCount() > 0:
-            self.removeRow(self.rowCount()-1)
-    def _copyRow(self):
-        self.insertRow(self.rowCount())
-        rowCount = self.rowCount()
-        columnCount = self.columnCount()
 
-        for j in range(columnCount):
-            if not self.item(rowCount-2,j) is None:
-                self.setItem(rowCount-1,j,QTableWidgetItem(self.item(rowCount-2,j).text()))
-
-class App(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.resize(1600, 600)
 
-        mainLayout = QHBoxLayout()
+        # setting title
+        self.setWindowTitle('Reflectometry of Quantum Materials (RQM)')
 
-        table = TableWidget()
-        mainLayout.addWidget(table)
+        # setting geometry
+        self.setGeometry(200,80,1000,600)
+
+        # calling method
+        # creating a combo box widget
+        self.layers = QComboBox(self)
+
+        # setting geometry of layers
+        self.layers.setGeometry(200, 150, 120, 30)
+        self.layers.move(31, 101)
+        self.layers.resize(98, 30)
+
+        # this is where the layers list will be initialized based on input sample info
+        my_list = ['Substrate', 'Layer 1', 'Layer 2', 'Layer 3']
+
+        self.layers.addItems(my_list)
+        self.layers.activated.connect(self.layerProperties)
+
+        self.createButtons()
+
+        # create table
+        self.elementTable = QTableWidget(self)
+        self.elementTable.move(150, 40)
+        self.elementTable.resize(660,125)
+
+        self.elementTable.setRowCount(3)
+        self.elementTable.setColumnCount(6)
+
+        self.elementTable.setHorizontalHeaderLabels(['Element', 'Thickness', 'Density', 'Roughness', 'Linked Roughness', 'Scattering Factor'])
 
 
-        buttonLayout = QVBoxLayout()
+    def createButtons(self):
+        # button used to add or remove layers
+        self.addlayer_button = QPushButton('Add Layer', self)
+        self.addlayer_button.resize(100, 32)
+        self.addlayer_button.move(30, 40)
+        self.addlayer_button.clicked.connect(self._addLayer)
 
-        button_new = QPushButton("New")
-        button_new.clicked.connect(table._addRow)
-        buttonLayout.addWidget(button_new)
+        self.removelayer_button = QPushButton('Remove Layer', self)
+        self.removelayer_button.resize(100, 32)
+        self.removelayer_button.move(30, 70)
+        self.removelayer_button.clicked.connect(self._removeLayer)
 
-        button_remove = QPushButton("Remove")
-        button_remove.clicked.connect(table._removeRow)
-        buttonLayout.addWidget(button_remove)
+        self.struct_button = QPushButton('Structural', self)
+        self.struct_button.resize(100, 32)
+        self.struct_button.move(150, 5)
 
-        button_copy = QPushButton("Copy")
-        button_copy.clicked.connect(table._copyRow)
-        buttonLayout.addWidget(button_copy, alignment=Qt.AlignTop)
+        self.poly_button = QPushButton('Element Variation', self)
+        self.poly_button.resize(100, 32)
+        self.poly_button.move(250, 5)
 
-        mainLayout.addLayout(buttonLayout)
-        self.setLayout(mainLayout)
+        self.mag_button = QPushButton('Magnetic', self)
+        self.mag_button.resize(100, 32)
+        self.mag_button.move(350, 5)
+
+        self.elementMode_button = QPushButton('Element', self)
+        self.elementMode_button.resize(100, 32)
+        self.elementMode_button.move(150, 175)
+
+        self.compoundMode_button = QPushButton('Compound', self)
+        self.compoundMode_button.resize(100, 32)
+        self.compoundMode_button.move(250, 175)
+
+    def _addLayer(self):
+        num = self.layers.count()
+        if num == 0:
+            self.layers.addItem('Substrate')
+        else:
+            self.layers.addItem('Layer ' + str(num))
+
+    def _removeLayer(self):
+        num = self.layers.count()
+
+        if num != 0:
+            self.layers.removeItem(num-1)
+
+
+    def layerProperties(self, index):
+            print('Activated: ' + str(index))
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    app.setStyleSheet('QPushButton{font-size: 20px; width: 200px; height: 50px}')
-    demo = App()
-    demo.show()
+    w = MainWindow()
+    w.show()
     sys.exit(app.exec_())
