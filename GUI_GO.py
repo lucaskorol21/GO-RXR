ec01d7f21902bd4e9c8ced5dc9b117e9a175f4ae
lucaskorol21 <74730122+lucaskorol21@users.noreply.github.com>
2022-09-07 11:48:07 -0600

    Starting on sample widget

--- GUI_GO.py
+++ GUI_GO.py
@@ -1,9 +1,110 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import Qt
+
 import sys
 
+
+class sampleWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        pagelayout = QHBoxLayout()
+
+        cblayout = QVBoxLayout()
+
+        addlayerButton = QPushButton('Add Layer')
+        copylayerButton = QPushButton('Copy Layer')
+        deletelayerButton = QPushButton('Delete Layer')
+
+
+        self.layerBox = QComboBox(self)
+        self.layerBox.addItems(['Substrate', 'Layer 1', 'Layer 2', 'Layer 3'])
+
+        # buttons
+        cblayout.addWidget(addlayerButton)
+        cblayout.addWidget(copylayerButton)
+        cblayout.addWidget(deletelayerButton)
+
+        # layer combo box
+        cblayout.addWidget(self.layerBox)
+
+        # table widget
+        # create table
+        self.elementTable = QTableWidget(self)
+        #self.elementTable.resize(660, 125)
+        self.elementTable.setRowCount(3)
+        self.elementTable.setColumnCount(6)
+
+        self.elementTable.setHorizontalHeaderLabels(
+            ['Element', 'Thickness', 'Density', 'Roughness', 'Linked Roughness', 'Scattering Factor'])
+
+        selectlayout = QVBoxLayout()
+
+        structButton = QPushButton('Structure')
+        selectlayout.addWidget(structButton)
+        polyButton = QPushButton('Element Variation')
+        selectlayout.addWidget(polyButton)
+        magButton = QPushButton('Magnetic')
+        selectlayout.addWidget(magButton)
+
+        pagelayout.addLayout(cblayout)
+        pagelayout.addWidget(self.elementTable)
+        pagelayout.addLayout(selectlayout)
+
+        self.setLayout(pagelayout)
+
+class ReflectometryApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # set the title
+        self.setWindowTitle('Reflectometry of Quantum Materials')
+
+        # set the geometry of the window
+        self.setGeometry(200,80,1000,600)
+
+        pagelayout = QVBoxLayout()
+        buttonlayout = QHBoxLayout()
+        self.stackedlayout = QStackedLayout()
+
+        pagelayout.addLayout(buttonlayout)
+        pagelayout.addLayout(self.stackedlayout)
+
+        label1 = QLabel('Label 1')
+        label2 = QLabel('Label 2')
+        label3 = QLabel('Label 3')
+
+        _sampleWidget = sampleWidget()  # initialize the sample widget
+        sampleButton = QPushButton('Sample')
+        sampleButton.clicked.connect(self.activate_tab_1)
+        buttonlayout.addWidget(sampleButton)
+        self.stackedlayout.addWidget(_sampleWidget)
+
+        reflButton = QPushButton('Reflectivity')
+        reflButton.clicked.connect(self.activate_tab_2)
+        buttonlayout.addWidget(reflButton)
+        self.stackedlayout.addWidget(label2)
+
+        goButton = QPushButton('Global Optimization')
+        goButton.clicked.connect(self.activate_tab_3)
+        buttonlayout.addWidget(goButton)
+        self.stackedlayout.addWidget(label3)
+
+        widget = QWidget()
+        widget.setLayout(pagelayout)
+        self.setCentralWidget(widget)
+
+    def activate_tab_1(self):
+        self.stackedlayout.setCurrentIndex(0)
+    def activate_tab_2(self):
+        self.stackedlayout.setCurrentIndex(1)
+    def activate_tab_3(self):
+        self.stackedlayout.setCurrentIndex(2)
+
+
 # Element table widget
+
+### Garbage code for layer --------------------------------------------------------------------------------------------
 class elementTable(QTableWidget):
     def __init__(self):
         super().__init__(3,5)
@@ -13,7 +114,8 @@ class compoundTable(QTableWidget):
     def __init__(self):
         super().__init__(1, 5)
 
-class MainWindow(QMainWindow):
+
+class TestWindow(QWidget):
     def __init__(self):
         super().__init__()
 
@@ -29,7 +131,7 @@ class MainWindow(QMainWindow):
 
         # setting geometry of layers
         self.layers.setGeometry(200, 150, 120, 30)
-        self.layers.move(31, 101)
+        self.layers.move(31, 131)
         self.layers.resize(98, 30)
 
         # this is where the layers list will be initialized based on input sample info
@@ -38,29 +140,26 @@ class MainWindow(QMainWindow):
         self.layers.addItems(my_list)
         self.layers.activated.connect(self.layerProperties)
 
-        self.createButtons()
+        self._createButtons()
 
-        # create table
-        self.elementTable = QTableWidget(self)
-        self.elementTable.move(150, 40)
-        self.elementTable.resize(660,125)
+        self._createElementTable()
+        
 
-        self.elementTable.setRowCount(3)
-        self.elementTable.setColumnCount(6)
-
-        self.elementTable.setHorizontalHeaderLabels(['Element', 'Thickness', 'Density', 'Roughness', 'Linked Roughness', 'Scattering Factor'])
-
-
-    def createButtons(self):
+    def _createButtons(self):
         # button used to add or remove layers
         self.addlayer_button = QPushButton('Add Layer', self)
         self.addlayer_button.resize(100, 32)
         self.addlayer_button.move(30, 40)
         self.addlayer_button.clicked.connect(self._addLayer)
 
+        self.copylayer_button = QPushButton('Copy Layer', self)
+        self.copylayer_button.resize(100, 32)
+        self.copylayer_button.move(30, 70)
+        self.copylayer_button.clicked.connect(self._copyLayer)
+
         self.removelayer_button = QPushButton('Remove Layer', self)
         self.removelayer_button.resize(100, 32)
-        self.removelayer_button.move(30, 70)
+        self.removelayer_button.move(30, 100)
         self.removelayer_button.clicked.connect(self._removeLayer)
 
         self.struct_button = QPushButton('Structural', self)
@@ -83,6 +182,18 @@ class MainWindow(QMainWindow):
         self.compoundMode_button.resize(100, 32)
         self.compoundMode_button.move(250, 175)
 
+    def _createElementTable(self):
+        # create table
+        self.elementTable = QTableWidget(self)
+        self.elementTable.move(150, 40)
+        self.elementTable.resize(660, 125)
+
+        self.elementTable.setRowCount(3)
+        self.elementTable.setColumnCount(6)
+
+        self.elementTable.setHorizontalHeaderLabels(
+            ['Element', 'Thickness', 'Density', 'Roughness', 'Linked Roughness', 'Scattering Factor'])
+
     def _addLayer(self):
         num = self.layers.count()
         if num == 0:
@@ -96,6 +207,13 @@ class MainWindow(QMainWindow):
         if num != 0:
             self.layers.removeItem(num-1)
 
+    def _copyLayer(self):
+        num = self.layers.count()
+        if num == 0:
+            self.layers.addItem('Substrate')
+        else:
+            self.layers.addItem('Layer ' + str(num))
+
 
     def layerProperties(self, index):
             print('Activated: ' + str(index))
@@ -103,6 +221,6 @@ class MainWindow(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = MainWindow()
-    w.show()
+    demo = ReflectometryApp()
+    demo.show()
     sys.exit(app.exec_())
