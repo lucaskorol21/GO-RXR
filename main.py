41c7626e28a948a7a7cb60753202b08a6cca7201
lucaskorol21 <74730122+lucaskorol21@users.noreply.github.com>
2023-01-03 10:27:20 -0600

    Testing out data smoothing algorithms

--- main.py
+++ main.py
@@ -4,26 +4,45 @@ import matplotlib.pyplot as plt
 import global_optimization as go
 from scipy.signal import *
 import copy
+from scipy import interpolate
+
+
+from scipy.fft import fft, fftfreq, fftshift, ifft
+
+
+
+
+def noise_removal(qz, R, s=1, k=3):
+    tck = interpolate.splrep(qz,R, s=s, k=k)
+    return interpolate.splev(qz, tck, der=0)
+
+
+
 
 
 if __name__ == '__main__':
-    fname = "//cabinet/work$/lsk601/My Documents/LSMO_For_Lucas/RXR_Twente-EM1-150K_v3.h5"
+    fname = "//cabinet/work$/lsk601/My Documents/LSMO_For_Lucas/RXR_Twente-EM1-150K_v5.h5"
 
     sample = ds.ReadSampleHDF5(fname)
 
     thickness, density, density_magnetic = sample.density_profile()
 
-    keys = ['Ti','O','C1','C2','Sr','La','Mn']
-
     density['Mn'] = density['Mn2+'] + density['Mn3+']
-    density['Sr'] = copy.copy(density['Sr_f'])
-    density['La0.7Sr0.3'] = density['Sr'] + density['La']
+    density['Sr/La'] = density['La'] + density['Sr']
 
-    plt.figure(1)
-    for key in keys:
-        plt.plot(thickness,density[key])
+    keys = ['Ti', 'O', 'C1','C2','Sr/La','Mn']
+
+    plt.figure()
+    for name in keys:
+        plt.plot(thickness,density[name])
 
     plt.legend(keys)
-    plt.ylabel('Reflectivity, R')
-    plt.xlabel('Depth (Angstrom)')
+    # plt.legend(my_legend, loc='center left', bbox_to_anchor=(1.02, 0.5))
+    plt.xlabel('Thickness (Angstrom)')
+    plt.ylabel('Density (mol/cm^3)')
     plt.show()
+
+    print(density.keys())
+
+
+
