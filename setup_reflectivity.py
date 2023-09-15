586f7d873de6d3730641f0064c383a34502b828a
lucaskorol21 <74730122+lucaskorol21@users.noreply.github.com>
2023-09-15 13:59:44 -0600

    correct setup file for Pythonrefletivity

--- setup_reflectivity.py
+++ setup_reflectivity.py
@@ -1,13 +1,11 @@
-from setuptools import setup, Extension
-from Cython.Build import cythonize
 import numpy
 
-setup(
-    ext_modules=cythonize(Extension(
-           "Pythonreflectivity",                                # the extension name
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+setup(ext_modules = cythonize(Extension(
+           "Pythonreflectivity",                                # the extesion name
            sources=["Pythonreflectivity.pyx"],
            language="c++",
            include_dirs=[numpy.get_include()]
-      ))
-
-)
+      )))
